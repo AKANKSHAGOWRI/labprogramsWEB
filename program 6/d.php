1299aade64fdff4a0ac91f9f4e585206631b50ad
AKANKSHAGOWRI <akankshagowri@gmail.com>
2020-12-19 20:47:41 +0530

    program 6 with ss

---
+++ program 6/d.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>Visitors count</head>
+
+<body>
+    <h1> WELCOME TO MY PAGE </h1>
+    <?php
+    print "<h3> REFRESH PAGE </h3>";
+    $name = "counter.txt";
+    $file = fopen($name, "r");
+    $hits = fscanf($file, "%d");
+    fclose($file);
+    $hits[0]++;
+    $file = fopen($name, "w");
+    fprintf($file, "%d", $hits[0]);
+    fclose($file);
+    print "Total number of views: " . $hits[0];
+    ?>
+</body>
+
+</html>
